b8c27e7f07499e0be27e1c4b445bca9d3bdce085
warthog33 <dave.mcg2@gmail.com>
2019-12-02 09:19:41 +1100

    Create nfcsnoop.h

---
+++ nfcsnoop.h
@@ -0,0 +1,26 @@
+/** 
+ * Structure of records stored in the default AOSP implementation of the NFC log
+ */
+#define _DEBUG_NFCSNOOP_
+
+#include <stdint.h>
+
+
+#define NFCSNOOZ_CURRENT_VERSION 0x01
+
+// The preamble is stored un-encrypted as the first part
+// of the file.
+typedef struct nfcsnooz_preamble_t {
+  uint8_t version;
+  uint64_t last_timestamp_ms;
+} __attribute__((__packed__)) nfcsnooz_preamble_t;
+
+// One header for each NCI packet
+typedef struct nfcsnooz_header_t {
+  uint16_t length;
+  uint32_t delta_time_ms;
+  uint8_t is_received;
+} __attribute__((__packed__)) nfcsnooz_header_t;
+
+#endif /* _DEBUG_NFCSNOOP_ */
+                                                                                                                                                                         38,1          Bot
